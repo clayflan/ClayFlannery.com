ca889d1a28b858fffecce563c60d8992c620fe4f
Clay Flannery <johndoe@example.comcflannery94@gmail.com>
2021-10-14 19:40:11 -0400

    bookshelf

---
+++ index.js
@@ -0,0 +1,22 @@
+$(function() {
+    $(window).scroll(function() {
+      var winTop = $(window).scrollTop();
+      if (winTop >= 30) {
+        $("body").addClass("sticky-shrinknav-wrapper");
+      } else{
+        $("body").removeClass("sticky-shrinknav-wrapper");
+      }
+    });
+  });
+  
+  //logo shirnk on scroll
+
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+      document.getElementById("logo").style.fontSize = "2em";
+    } else {
+      document.getElementById("logo").style.fontSize = "5em";
+    }
+  }
