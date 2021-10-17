057b942b72c8ca48cd4a3e1d6f374fbad386ac78
Clay Flannery <johndoe@example.comcflannery94@gmail.com>
2021-10-17 13:37:58 -0400

    ship shape

---
+++ header.php
@@ -0,0 +1,43 @@
+<div class="bg">            
+                
+    </div>
+
+    <div class="container">
+
+        <div class="intro">
+            
+            <h1 class="logo" id="logo">&nbsp;Clay Flannery &nbsp; </h1>
+            <!-- <img class="jarret" src="/Media/jarretbay.jpg" alt="JB"> -->
+
+        </div>
+        
+
+        <div class="pageHeader">
+            
+            <nav id="nav" class="navbar navbar-expand-md navbar-light">
+                <a href="style1.html" class="navbar-brand">
+<!--                     <img class="logo  align-bottom" src="1620221-beverage/png/001-barrel.png" alt=logo width="60" height="auto">
+ -->
+                    <i class="fa-thin fa-whale"></i>
+                    <h1>Clay Flannery</h1>
+                </a>
+                <button class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#toggleMobileMenu"
+                        aria-controls="toggleMobileMenu"
+                        aria-expand="false"
+                        aria-lable="Toggle Navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="toggleMobileMenu">
+                    <ul class="navbar-nav ms-auto text-center">
+                        <li>
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>    
+                </div>
+            </nav>
+        </div>
+    </div>
