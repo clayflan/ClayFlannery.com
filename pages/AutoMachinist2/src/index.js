40315b4a09713a2707b6f0cbbefb68abea17880b
Clayflan <cflannery94@gmail.com>
2024-09-28 03:49:59 -0400

    8

--- pages/AutoMachinist2/src/index.js
+++ pages/AutoMachinist2/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import '../styles/index.css';
-<script src="https://kit.fontawesome.com/your-fontawesome-kit.js" crossorigin="anonymous"></script>
 
 const container = document.getElementById('root');
 const root = createRoot(container);
