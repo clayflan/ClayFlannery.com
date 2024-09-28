66e47c813b82aebbd2eecb4a22677a6e9cc43614
Clayflan <cflannery94@gmail.com>
2024-09-28 03:40:48 -0400

    3

--- pages/AutoMachinist2/src/components/App.js
+++ pages/AutoMachinist2/src/components/App.js
@@ -8,7 +8,6 @@ import Pricing from '../pages/Pricing';
 import LogIn from '../pages/LogIn';
 import SignUp from '../pages/SignUp';
 import '../styles/App.css';
-<script src="https://kit.fontawesome.com/your-fontawesome-kit.js" crossorigin="anonymous"></script>
 
 function App() {
 
