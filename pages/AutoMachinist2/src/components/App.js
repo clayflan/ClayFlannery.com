b10d332de2c6ab86bda8c6c25e737e7c4791513a
Clay Flannery <johndoe@example.comcflannery94@gmail.com>
2024-09-28 06:17:41 -0400

    2

--- pages/AutoMachinist2/src/components/App.js
+++ pages/AutoMachinist2/src/components/App.js
@@ -27,7 +27,7 @@ function App() {
   }, []);
 
   return (
-    <Router basename="/pages/automachinist2/">  {/* Removed the basename for now */}
+    <Router basename="/pages/automachinist2/src/pages">  {/* Removed the basename for now */}
       <div className="App">
         <Header />
         <Routes>
