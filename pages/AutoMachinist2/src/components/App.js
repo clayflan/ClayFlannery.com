a0412f4db1196fee092bdab929932a95fdeca6b4
Clayflan <cflannery94@gmail.com>
2024-09-28 01:54:23 -0400

    8

--- pages/AutoMachinist2/src/components/App.js
+++ pages/AutoMachinist2/src/components/App.js
@@ -12,7 +12,7 @@ import './styles/App.css';
 
 function App() {
   return (
-    <Router basename='{process.env.PUBLIC_URL}'>
+    <Router basename='Clayflannery.com/pages/AutoMachinist2/src/pages'>
       <div className="App">
         <Header />
         <Routes>
