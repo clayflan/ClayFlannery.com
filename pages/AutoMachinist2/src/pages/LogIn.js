b915cca7cdd3c2577996fdd4224918d72b4e4fe5
Clayflan <cflannery94@gmail.com>
2024-09-28 01:17:44 -0400

    0

--- pages/AutoMachinist2/src/pages/LogIn.js
+++ pages/AutoMachinist2/src/pages/LogIn.js
@@ -4,22 +4,29 @@ import './styles/App.css';
 
 const LogIn = () => {
   return (
-    <div className="login">
+    <><div className="login">
       <h1>Log In</h1>
       <form>
         <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" required />
-        </div>
-        <button type="submit">Log In</button>
-      </form>
-      <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
-      <p><Link to="/">Forgot Password?</Link></p>
-    </div>
+          <section id="services" className="services">
+            <h2>Our Expertise</h2>
+            <div className="service-grid">
+              <div className="service-item">
+                <i className="fas fa-cogs"></i>
+                <h3>Precision Machining</h3>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" required />
+              </div>
+              <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" required />
+              </div>
+              <button type="submit">Log In</button>
+            </div>
+          </section>
+          </div>
+      </form><p>Don't have an account? <Link to="/signup">Sign Up</Link></p><p><Link to="/">Forgot Password?</Link></p></>
+    </>
   );
 };
 
