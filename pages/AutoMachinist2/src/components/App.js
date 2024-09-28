4ee23c3e389f515e96ad06eb7d1c7b616acfae9c
Clayflan <cflannery94@gmail.com>
2024-09-27 21:57:35 -0400

    36

--- pages/AutoMachinist2/src/components/App.js
+++ pages/AutoMachinist2/src/components/App.js
@@ -14,7 +14,7 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/AutoMachinist" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/AITools" element={<AITools />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/login" element={<LogIn />} />
